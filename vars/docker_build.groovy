1efb2512e09079b737d92c18c3d4adac1e8afdde
YASHA-GOYAL <yashagoyal7@gmail.com>
2026-05-09 22:17:49 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,5 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser) {
   echo 'this is Building the code.'
-  sh 'docker build -t ${DockerHubUse}/${ProjectName}:${ImageTag}'
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
   echo 'docker build successfully.'
 }
